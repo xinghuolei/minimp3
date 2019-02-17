f73c1b422c7ea07bb7a258bdaed0b6735abb04e4
忘却浮生 <xinghuolei@163.com>
2019-02-17 18:05:34 +0800

    fix go routine leak

--- decode.go
+++ decode.go
@@ -14,9 +14,9 @@ int decode(mp3dec_t *dec, mp3dec_frame_info_t *info, unsigned char *data, int *l
     samples = mp3dec_decode_frame(dec, data, *length, pcm, info);
     *decoded_length = samples * info->channels * sizeof(mp3d_sample_t);
     *length -= info->frame_bytes;
-    unsigned char buffer[samples * info->channels * sizeof(mp3d_sample_t)];
-    memcpy(buffer, (unsigned char*)&(pcm), sizeof(mp3d_sample_t) * samples * info->channels);
-    memcpy(decoded, buffer, sizeof(mp3d_sample_t) * samples * info->channels);
+    unsigned char buffer[*decoded_length];
+    memcpy(buffer, (unsigned char*)&(pcm), *decoded_length);
+    memcpy(decoded, buffer, *decoded_length);
     return info->frame_bytes;
 }
 */
@@ -27,10 +27,11 @@ import (
 	"sync"
 	"time"
 	"unsafe"
+	"fmt"
 )
 
 const maxSamplesPerFrame = 1152 * 2
-
+const pointSize = 4 // float32 4  int16 2
 // Decoder decode the mp3 stream by minimp3
 type Decoder struct {
 	readerLocker  *sync.Mutex
@@ -48,7 +49,7 @@ type Decoder struct {
 }
 
 // BufferSize Decoded data buffer size.
-var BufferSize = 1024 * 10
+var BufferSize = 1024 * 100
 
 // WaitForDataDuration wait for the data time duration.
 var WaitForDataDuration = time.Millisecond * 10
@@ -66,7 +67,7 @@ func NewDecoder(reader io.Reader) (dec *Decoder, err error) {
 		for {
 			select {
 			case <-dec.context.Done():
-				break
+				return
 			default:
 			}
 			if len(dec.data) > BufferSize {
@@ -92,17 +93,17 @@ func NewDecoder(reader io.Reader) (dec *Decoder, err error) {
 		for {
 			select {
 			case <-dec.context.Done():
-				break
+				return
 			default:
 			}
 			if len(dec.decodedData) > BufferSize {
 				<-time.After(WaitForDataDuration)
 				continue
 			}
-			var decoded = [maxSamplesPerFrame * 4]byte{}
+			var decoded = [maxSamplesPerFrame * pointSize]byte{}
 			var decodedLength = C.int(0)
 			var length = C.int(len(dec.data))
-			if len(dec.data) == 0 {
+			if len(dec.data) < 960 {//at least a frame
 				<-time.After(WaitForDataDuration)
 				continue
 			}
@@ -111,6 +112,10 @@ func NewDecoder(reader io.Reader) (dec *Decoder, err error) {
 				&length, (*C.uchar)(unsafe.Pointer(&decoded[0])),
 				&decodedLength)
 			if int(frameSize) == 0 {
+				<-time.After(WaitForDataDuration)
+				continue
+			} else if int(frameSize) == -1{
+				fmt.Println("decode error")
 				<-time.After(WaitForDataDuration)
 				continue
 			}
@@ -179,7 +184,7 @@ func DecodeFull(mp3 []byte) (dec *Decoder, decodedData []byte, err error) {
 	info := C.mp3dec_frame_info_t{}
 	var length = C.int(len(mp3))
 	for {
-		var decoded = [maxSamplesPerFrame * 4]byte{}
+		var decoded = [maxSamplesPerFrame * pointSize]byte{}
 		var decodedLength = C.int(0)
 		frameSize := C.decode(&dec.decode,
 			&info, (*C.uchar)(unsafe.Pointer(&mp3[0])),
